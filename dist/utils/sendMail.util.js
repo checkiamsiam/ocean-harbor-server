e55231b591c7801fb16e70b5a42b02a20555dd2f
checkiamsiam <issiam02415@gmail.com>
2024-01-24 11:35:54 +0600

    Refactor sendMail.util.js and sendMail.util.ts

--- dist/utils/sendMail.util.js
+++ dist/utils/sendMail.util.js
@@ -13,27 +13,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_status_1 = __importDefault(require("http-status"));
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const config_1 = __importDefault(require("../config"));
 const customError_util_1 = __importDefault(require("./customError.util"));
 const sendEmail = (options) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // const nodeMailerOptions = {
-        //   host: config.mailtrap.host,
-        //   port: parseInt(config.mailtrap.port),
-        //   auth: {
-        //     user: config.mailtrap.auth.user,
-        //     pass: config.mailtrap.auth.pass,
-        //   },
-        //   domain: config.mailtrap.domain,
-        // };
-        // // 1. create transporter
-        // const transporter = nodemailer.createTransport(nodeMailerOptions);
-        // // 2. define email options
-        // const mailOptions = {
-        //   from: config.mailtrap.sendingEmail,
-        //   ...options,
-        // };
-        // // 3. send email
-        // await transporter.sendMail(mailOptions);
+        const nodeMailerOptions = {
+            host: config_1.default.mailtrap.host,
+            port: parseInt(config_1.default.mailtrap.port),
+            auth: {
+                user: config_1.default.mailtrap.auth.user,
+                pass: config_1.default.mailtrap.auth.pass,
+            },
+            domain: config_1.default.mailtrap.domain,
+        };
+        // 1. create transporter
+        const transporter = nodemailer_1.default.createTransport(nodeMailerOptions);
+        // 2. define email options
+        const mailOptions = Object.assign({ from: config_1.default.mailtrap.sendingEmail }, options);
+        // 3. send email
+        yield transporter.sendMail(mailOptions);
     }
     catch (error) {
         throw new customError_util_1.default("There was an error sending the email. Try again later!", http_status_1.default.INTERNAL_SERVER_ERROR);
--- src/utils/sendMail.util.ts
+++ src/utils/sendMail.util.ts
@@ -5,24 +5,24 @@ import AppError from "./customError.util";
 
 const sendEmail = async (options: Omit<SendMailOptions, "from">) => {
   try {
-    // const nodeMailerOptions = {
-    //   host: config.mailtrap.host,
-    //   port: parseInt(config.mailtrap.port),
-    //   auth: {
-    //     user: config.mailtrap.auth.user,
-    //     pass: config.mailtrap.auth.pass,
-    //   },
-    //   domain: config.mailtrap.domain,
-    // };
-    // // 1. create transporter
-    // const transporter = nodemailer.createTransport(nodeMailerOptions);
-    // // 2. define email options
-    // const mailOptions = {
-    //   from: config.mailtrap.sendingEmail,
-    //   ...options,
-    // };
-    // // 3. send email
-    // await transporter.sendMail(mailOptions);
+    const nodeMailerOptions = {
+      host: config.mailtrap.host,
+      port: parseInt(config.mailtrap.port),
+      auth: {
+        user: config.mailtrap.auth.user,
+        pass: config.mailtrap.auth.pass,
+      },
+      domain: config.mailtrap.domain,
+    };
+    // 1. create transporter
+    const transporter = nodemailer.createTransport(nodeMailerOptions);
+    // 2. define email options
+    const mailOptions = {
+      from: config.mailtrap.sendingEmail,
+      ...options,
+    };
+    // 3. send email
+    await transporter.sendMail(mailOptions);
   } catch (error) {
     throw new AppError(
       "There was an error sending the email. Try again later!",
